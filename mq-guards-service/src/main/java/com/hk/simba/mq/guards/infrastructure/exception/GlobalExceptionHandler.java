97b743d63cb1d14393c75aeaa5891b75322f9350
Allen <hongxq@homeking365.com>
2021-07-15 09:58:37 +0800

    REQ-7 消息补发中心代码提交

--- mq-guards-service/src/main/java/com/hk/simba/mq/guards/infrastructure/exception/GlobalExceptionHandler.java
+++ mq-guards-service/src/main/java/com/hk/simba/mq/guards/infrastructure/exception/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public class GlobalExceptionHandler {
 
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public BaseResponse handleBizException(Exception ex) {
