9d9623c120795c1b3478a1cb3ca02a4a3205cc0b
Allen <hongxq@homeking365.com>
2021-07-15 10:07:42 +0800

    REQ-7 消息补发中心代码提交

---
+++ mq-guards-service/src/test/java/com/hk/simba/mq/guards/SampleTest.java
@@ -0,0 +1,12 @@
+package com.hk.simba.mq.guards;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("com.hk.quark.base.testing.category.SmallTests")
+public class SampleTest {
+
+    @Test
+    public void test() {
+    }
+}
