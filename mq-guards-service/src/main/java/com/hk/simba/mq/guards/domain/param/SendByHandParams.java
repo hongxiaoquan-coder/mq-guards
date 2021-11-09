9d6a4a714c4269da31d478e2074c9a225184156b
Allen <hongxq@homeking365.com>
2021-11-09 16:57:04 +0800

    fix-simba3-hxq 修改手动补发接口入参时间格式

--- mq-guards-service/src/main/java/com/hk/simba/mq/guards/domain/param/SendByHandParams.java
+++ mq-guards-service/src/main/java/com/hk/simba/mq/guards/domain/param/SendByHandParams.java
@@ -40,13 +40,13 @@ public class SendByHandParams implements Serializable {
     /**
      * 开始时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date beginTime;
 
     /**
      * 结束时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date endTime;
 
 }
